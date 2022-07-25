b88ad01f671f96f316235916c5b56dc4eadf3b75
Dj2035 <dk.jude@yahoo.com>
2022-07-25 10:22:04 +0200

    Imported propTypes and and used it to enforce data types on the MovieView card

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 export class MovieView extends React.Component {
 
@@ -8,7 +9,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.ImagePath} crossOrigin="true" />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
@@ -22,12 +23,38 @@ export class MovieView extends React.Component {
           <span className="label">Actors: </span>
           <span className="value">{movie.Actors}</span>
         </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="genre-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Genre.Description}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="director-bio">
+          <span className="label">Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div>
+        <div className="director-birthyear">
+          <span className="label">BirthYear: </span>
+          <span className="value">{movie.Director.Birth}</span>
+        </div>
+        <div className="director-deathyear">
+          <span className="label">DeathYear: </span>
+          <span className="value">{movie.Director.Death}</span>
+        </div>
         <div className="movie-release-year">
           <span className="label">Release year: </span>
           <span className="value">{movie.ReleaseYear}</span>
         </div>
+        <div className="movie-button-div">
+          <button onClick={() => { onBackClick(null); }} >Back</button>
+        </div>
 
-        <button onClick={() => { onBackClick(null); }} >Back</button>
 
       </div>
     );
@@ -39,9 +66,19 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    Actors: PropTypes.string,
-    ReleaseYear: PropTypes.string
+    Actors: PropTypes.array.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      BirthYear: PropTypes.string,
+      DeathYear: PropTypes.string
+    }),
+    ReleaseYear: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
   }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };
