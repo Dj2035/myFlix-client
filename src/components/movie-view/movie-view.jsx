74c948827323d83f04b82080eeade41ac32289e1
Dj2035 <dk.jude@yahoo.com>
2022-08-11 11:04:17 +0200

    Update movie-view component to display movie details

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -17,42 +17,36 @@ export class MovieView extends React.Component {
             <img src={movie.ImagePath} crossOrigin="true" />
           </Col>
         </Row>
+
         <Row className="movie-title mb-2">
           <Col className="label" xs lg="2">Title: </Col>
           <Col className="value">{movie.Title}</Col>
         </Row>
+
         <Row className="movie-description mb-2">
           <Col className="label" xs lg="2">Description: </Col>
           <Col className="value">{movie.Description}</Col>
         </Row>
+
         <Row className="movie-actors mb-2">
           <Col className="label" xs lg="2">Actors: </Col>
           <Col className="value">{movie.Actors}</Col>
         </Row>
-        <Row className="movie-genre mb-1">
+
+        <Row className="movie-genre mb-2">
           <Col className="label" xs lg="2">Genre: </Col>
-          <Col className="value">{movie.Genre.Name}</Col>
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">{movie.Genre.Name}</Button>
+          </Link>
         </Row>
-        <Row className="genre-description mb-2">
+
+        <Row className="movie-director mb-2">
           <Col className="label" xs lg="2">Description: </Col>
-          <Col className="value">{movie.Genre.Description}</Col>
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button variant="link">{movie.Genre.Description}</Button>
+          </Link>
         </Row>
-        <Row className="movie-director mb-1">
-          <Col className="label" xs lg="2">Director: </Col>
-          <Col className="value">{movie.Director.Name}</Col>
-        </Row>
-        <Row className="director-bio mb-1">
-          <Col className="label" xs lg="2">Bio: </Col>
-          <Col className="value">{movie.Director.Bio}</Col>
-        </Row>
-        <Row className="director-birthyear mb-1">
-          <Col className="label" xs lg="2">BirthYear: </Col>
-          <Col className="value">{movie.Director.Birth}</Col>
-        </Row>
-        <Row className="director-deathyear mb-2">
-          <Col className="label" xs lg="2">DeathYear: </Col>
-          <Col className="value">{movie.Director.Death}</Col>
-        </Row>
+
         <Row className="movie-release-year mb-2">
           <Col className="label" xs lg="2">Release year: </Col>
           <Col className="value">{movie.ReleaseYear}</Col>
