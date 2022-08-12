0642233d5d12c35f824984140e449eeca6d63e74
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:15:03 +0200

    Cut out the user-info component

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function UserInfo({ email, username }) {
+function UserInfo(props) {
+  const { user } = props;
   return (
     <>
       <h4>Your Info</h4>
-      <p>Username: {username}</p>
-      <p>Email: {email}</p>
+      <p>Username: {user.Username}</p>
+      <p>Email: {user.Email}</p>
     </>
   )
 }
