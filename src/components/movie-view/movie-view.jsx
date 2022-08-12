82ef695e2ca70b9dc7d0ed317b1c34ecddd48287
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:11:10 +0200

    added addMovie function and button

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,59 +1,90 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import { Button, Container, Row, Col, Accordion } from "react-bootstrap";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
 import "./movie-view.scss"
 
 export class MovieView extends React.Component {
+
+  addMovie = (movieId) => {
+    let username = localStorage.getItem("user");
+    let token = localStorage.getItem("token");
+
+    axios.post(
+      `https://jude-movie-api.herokuapp.com/users/${username}/movies/${movieId}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response.data);
+        alert(`${movie.Title} has been added from your list.`);
+        window.open('/users/:username', '_self');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
       <Container className="movie-view">
-        <Row className="movie-poster mb-3">
-          <Col>
-            <img src={movie.ImagePath} crossOrigin="true" />
-          </Col>
-        </Row>
+        <Card>
+          <Card.Header>
+            <Card.Img variant="top" crossOrigin="true" src={movie.ImagePath} />
+          </Card.Header>
+          <Card.Body>
+            <Row className="movie-title mb-2">
+              <Col className="label" xs lg="3">Title: </Col>
+              <Col className="value" xs lg="9">{movie.Title}</Col>
+            </Row>
 
-        <Row className="movie-title mb-2">
-          <Col className="label" xs lg="2">Title: </Col>
-          <Col className="value">{movie.Title}</Col>
-        </Row>
+            <Row className="movie-description mb-2">
+              <Col className="label" xs lg="3">Description: </Col>
+              <Col className="value" xs lg="9">{movie.Description}</Col>
+            </Row>
 
-        <Row className="movie-description mb-2">
-          <Col className="label" xs lg="2">Description: </Col>
-          <Col className="value">{movie.Description}</Col>
-        </Row>
+            <Row className="movie-actors mb-2">
+              <Col className="label" xs lg="3">Actors: </Col>
+              <Col className="value" xs lg="9">{movie.Actors}</Col>
+            </Row>
 
-        <Row className="movie-actors mb-2">
-          <Col className="label" xs lg="2">Actors: </Col>
-          <Col className="value">{movie.Actors}</Col>
-        </Row>
+            <Row className="movie-genre mb-2">
+              <Col className="label" xs lg="3">Genre: </Col>
+              <Col xs lg="9">
+                <Link to={`/genres/${movie.Genre.Name}`}>
+                  <Button variant="link">{movie.Genre.Name}</Button>
+                </Link>
+              </Col>
+            </Row>
 
-        <Row className="movie-genre mb-2">
-          <Col className="label" xs lg="2">Genre: </Col>
-          <Link to={`/genres/${movie.Genre.Name}`}>
-            <Button variant="link">{movie.Genre.Name}</Button>
-          </Link>
-        </Row>
+            <Row className="movie-director mb-2">
+              <Col className="label" xs lg="3">Description: </Col>
+              <Col xs lg="9">
+                <Link to={`/directors/${movie.Director.Name}`}>
+                  <Button variant="link">{movie.Director.Name}</Button>
+                </Link>
+              </Col>
+            </Row>
 
-        <Row className="movie-director mb-2">
-          <Col className="label" xs lg="2">Description: </Col>
-          <Link to={`/directors/${movie.Director.Name}`}>
-            <Button variant="link">{movie.Genre.Description}</Button>
-          </Link>
-        </Row>
+            <Row className="movie-release-year mb-2">
+              <Col className="label" xs lg="3">Release year: </Col>
+              <Col className="value" xs lg="9">{movie.ReleaseYear}</Col>
+            </Row>
+          </Card.Body>
 
-        <Row className="movie-release-year mb-2">
-          <Col className="label" xs lg="2">Release year: </Col>
-          <Col className="value">{movie.ReleaseYear}</Col>
-        </Row>
-
-        <Button className="movie-button-div" onClick={() => { onBackClick(null); }} >Back</Button>
-      </Container>
+          <Card.Footer>
+            <Button className="button ml-2" onClick={() => { this.addMovie(movie, user); }}>
+              Add to favorites
+            </Button>
+            <Button className="button ml-2" onClick={() => { onBackClick(null); }} >
+              Back
+            </Button>
+          </Card.Footer>
+        </Card>
+      </Container >
     );
   }
 
