5db614bcddd8810566ddbc3e6def663d0f93af3e
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:14:31 +0200

    Added form user update info and validation

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,7 +1,75 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-function UpdateUser({ handleSubmit, handleUpdate }) {
+function UpdateUser(props) {
+  const { user } = props;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [values, setValues] = useState({
+    usernameErr: '',
+    passwordErr: '',
+    emailErr: '',
+  });
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setValues({ ...values, usernameErr: 'Username required' });
+      isReq = false;
+    } else if (username.length < 5) {
+      setValues({ ...values, usernameErr: 'Username must be at least 5 characters long' });
+      isReq = false;
+    }
+    if (!password) {
+      setValues({ ...values, passwordErr: 'Password required' });
+      isReq = false;
+    } else if (password.length < 6) {
+      setValues({ ...values, passwordErr: 'Password must be at least 6 characters long' });
+      isReq = false;
+    }
+    if (!email) {
+      setValues({ ...values, emailErr: 'Email required' });
+      isReq = false;
+    } else if (email.indexOf('@') === -1) {
+      setValues({ ...values, emailErr: 'Enter valid email' });
+      isReq = false;
+    }
+    return isReq;
+  }
+
+  // Update Profile
+  const handleUpdate = () => {
+    const isReq = validate();
+    if (isReq) {
+      const token = localStorage.getItem('token');
+      axios.put(`https://jude-movie-api.herokuapp.com/users/${user.Username}`,
+        {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+        .then((response) => {
+          alert('Your profile has been updated');
+          localStorage.setItem("user", response.data.Username),
+            console.log(response.data);
+          window.open('/', '_self');
+        })
+        .catch((e) => {
+          console.log('Error');
+          alert('Unable to update profile.');
+        });
+    }
+  };
+
   return (
     <Form className='profile-form'>
       <h2>Want to change some info?</h2>
@@ -9,52 +77,50 @@ function UpdateUser({ handleSubmit, handleUpdate }) {
         <Form.Label>Username:</Form.Label>
         <Form.Control
           type="text"
-          value={username}
           defaultValue={user.Username}
-          onChange={e => handleUpdate(e)}
+          onChange={e => setUsername(e.target.value)}
           required
           placeholder="Enter a username"
         />
+        {values.usernameErr && <p>{values.usernameErr}</p>}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formPassword">
         <Form.Label>Password:</Form.Label>
         <Form.Control
           type="password"
-          value={password}
-          defaultValue=""
-          onChange={e => handleUpdate(e)}
+          value="*******"
+          onChange={e => setPassword(e.target.value)}
           required
           minLength="6"
           placeholder="Your password must be 6 or more characters"
         />
+        {values.passwordErr && <p>{values.passwordErr}</p>}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formEmail">
         <Form.Label>Email:</Form.Label>
         <Form.Control
           type="email"
-          value={email}
           defaultValue={user.Email}
-          onChange={e => handleUpdate(e.target.value)}
+          onChange={e => setEmail(e.target.value)}
           required
           placeholder="Enter your email address"
         />
+        {values.emailErr && <p>{values.emailErr}</p>}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBirthday">
         <Form.Label>Birthday:</Form.Label>
         <Form.Control
           type="date"
-          value={birthday}
           defaultValue={user.Birthday}
-          onChange={e => handleUpdate(e.target.value)}
+          onChange={e => setBirthday(e.target.value)}
           required
         />
       </Form.Group>
-
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Update
+      <Button variant="primary" type="submit" onClick={handleUpdate}>
+        Update profile
       </Button>
     </Form>
   )
