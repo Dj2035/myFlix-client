15667a7163030615925880ec61eb076ac6f1415f
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:12:58 +0200

    Added profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,107 +1,64 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 
-import { UserInfo } from './user-info';
 import { FavMovies } from './favorite-movies';
 import { UpdateUser } from './update-user';
 
 
 
 import './profile-view.scss';
-import { useEffect } from "react";
 
-export function ProfileView({ movies, onUpdatedUserInfo }) {
-  const [user, setUser] = useStates({
+export function ProfileView(props) {
+  const [user, setUser] = useState(props.user);
+  const [movies, setMovies] = useState(props.movies);
+  const [favouriteMovies, setFavouriteMovies] = useState([]);
+  const currentUser = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
 
-  })
-
-  const favoriteMovieList = movies.filter((movies) => {
-
-  });
-
-  const getUserData = () => {
-    let user = localStorage.getItem("user");
-    let token = localStorage.getItem("token");
-    axios
-      .get(`https://jude-movie-api.herokuapp.com/users/${user}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        setUsername(response.data.Username);
-        setEmail(response.data.Email);
-        setUserData(response.data);
-        setFavoriteMoviesList(response.data.FavoriteMovies);
-        console.log(response);
-
-        response.data.FavoriteMovies.forEach((movie_id) => {
-          let favMovies = props.movies.filter(
-            (movie) => movie._id === movie_id
-          );
-          setMovies(favMovies);
-        });
+  const getUser = () => {
+    axios.get(`https://jude-movie-api.herokuapp.com/users/${currentUser}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        setUser(response.data);
+        setFavouriteMovies(response.data.FavouriteMovies)
       })
       .catch((error) => console.error(error));
   };
 
-  // Delete Profile
-  const handleDelete = (e) => {
-    const user = localStorage.getItem("user");
-    const token = localStorage.getItem("token");
-    axios.delete(`https://jude-movie-api.herokuapp.com/users/${user}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    alert(`The account ${user.Username} was successfully deleted.`);
-    localStorage.clear();
-    window.open("/register", "_self");
-  };
-
-
-  const handleSubmit = (e) => {
-
-  }
-  // Update Profile
-  const handleUpdate = () => {
-    let user = localStorage.getItem("user");
-    let token = localStorage.getItem("token");
-
-    axios
-      .put(
-        `https://jude-movie-api.herokuapp.com/users/${user}`,
-        {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday,
-        },
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-
-      .then((response) => {
-        alert("Your profile has been updated");
-        localStorage.setItem("user", response.data.Username),
-          console.log(response.data);
-        window.open("/", "_self");
-      })
-      .catch((e) => {
-        console.log("Error");
-      });
-  };
-
   useEffect(() => {
-    getUserData();
-  }, []);
+    getUser();
+  }, [])
+
+  // Delete Profile
+  const handleDelete = () => {
+    axios.delete(`https://jude-movie-api.herokuapp.com/users/${currentUser}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(() => {
+        alert(`The account ${user.Username} was successfully deleted.`)
+        localStorage.clear();
+        window.open('/register', '_self');
+      })
+      .catch(error => console.error(error))
+  };
 
   return (
     <Container>
       <Row>
         <Col xs={12} sm={4}>
           <Card>
+            <Card.Title>Your Info</Card.Title>
             <Card.Body>
-              <UserInfo username={user.Username} email={user.Email} />
+              <Row className="mb-2">
+                <Col className="label" xs lg="3">Username: </Col>
+                <Col className="value" xs lg="9">{user.Username}</Col>
+              </Row>
+              <Row className="mb-2">
+                <Col className="label" xs lg="3">Email: </Col>
+                <Col className="value" xs lg="9">{user.Email}</Col>
+              </Row>
             </Card.Body>
           </Card>
 
@@ -109,16 +66,22 @@ export function ProfileView({ movies, onUpdatedUserInfo }) {
         <Col xs={12} sm={8}>
           <Card>
             <Card.Body>
-              <UpdateUser handleSubmit={handleSubmit} hanleUpdate={handleUpdate} />
+              <UpdateUser user={user} />
+              <Button className="d-block mt-5" variant="danger" onClick={handleDelete}>Delete profile</Button>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-
-      <FavMovies favoriteMovieList={favoriteMovieList} />
-
-      <Button className="mt-2 ml-4" onClick={handleDelete}>
-        Delete your profile
+      <Row>
+        <FavMovies
+          movies={movies}
+          favouriteMovies={favouriteMovies}
+          currentUser={currentUser}
+          token={token}
+        />
+      </Row>
+      <Button className="button ml-2" onClick={() => { onBackClick(null); }} >
+        Back
       </Button>
 
     </Container >
