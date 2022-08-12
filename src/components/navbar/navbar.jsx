c1d80479c54f021d076a790f9c641afda530e6bb
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:11:44 +0200

    Update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./navbar.scss";
 
 export function Menubar({ user }) {
-
   const isAuth = () => {
     if (typeof window == "undefined") {
       return false;
@@ -16,23 +16,25 @@ export function Menubar({ user }) {
     }
   }
 
-  const onLoggedOut = () => {
+  const handleLogOut = (e) => {
+    e.preventDefault();
     localStorage.clear();
     window.open("/", "_self");
-  }
+    props.onLoggedOut(user);
+  };
 
   return (
-    <Navbar className="main-nav" sticky="top" expand="lg" bg="dark" variant="blue">
+    <Navbar className="main-nav" sticky="top" expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand className="navbar-logo" href="/">MyFlix</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             {isAuth() && (
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
             )}
             {isAuth() && (
-              <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
+              <Button variant="link" onClick={handleLogOut}>Logout</Button>
             )}
             {isAuth() && (
               <Nav.Link href="/">Sign-in</Nav.Link>
@@ -40,9 +42,7 @@ export function Menubar({ user }) {
             {isAuth() && (
               <Nav.Link href="/register">Sign-up</Nav.Link>
             )}
-            {isAuth() && (
-              <Button variant="link" onClick={onLoggedOut}>Logout</Button>
-            )}
+
           </Nav>
         </Navbar.Collapse>
       </Container>
