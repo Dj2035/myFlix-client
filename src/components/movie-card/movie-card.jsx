b970c2baf04d6ba0188d5be3eae49103a8b815f1
Dj2035 <dk.jude@yahoo.com>
2022-08-12 16:09:15 +0200

    Added crossorigin to imagePath

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -13,7 +13,7 @@ export class MovieCard extends React.Component {
 
     return (
       <Card className="movieCard text-center">
-        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Img crossOrigin="anonymous" variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Link to={`/movies/${movie._id}`}>
