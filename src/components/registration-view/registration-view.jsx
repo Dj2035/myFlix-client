24998c8663181614c002a21b1533b16184dc9727
Dj2035 <dk.jude@yahoo.com>
2022-08-16 17:32:58 +0200

    Added conatainer and modified button link

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import axios from "axios";
 
-import { Form, Button, Card, CardGroup } from "react-bootstrap";
+import { Form, Button, Card, CardGroup, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./registration-view.scss";
 
@@ -70,70 +71,72 @@ export function RegistrationView(props) {
   };
 
   return (
-    <CardGroup>
-      <Card>
-        <Card.Body>
-          <Card.Title className="text-center">Welcome to MyFlix</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
-          <Form>
-            <Form.Group className="reg-form-inputs mb-3" controlId="formUsername">
-              <Form.Label>Username:</Form.Label>
-              <Form.Control
-                type="text"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                required
-                placeholder="Enter username"
-              />
-              {usernameErr && <p>{usernameErr}</p>}
-            </Form.Group>
+    <Container className="registration-view">
+      <CardGroup>
+        <Card>
+          <Card.Body>
+            <Card.Title className="text-center">Welcome to MyFlix</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
+            <Form>
+              <Form.Group className="reg-form-inputs mb-3" controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  required
+                  placeholder="Enter username"
+                />
+                {usernameErr && <p>{usernameErr}</p>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPassword">
-              <Form.Label>Password:</Form.Label>
-              <Form.Control
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                required
-                minLength="6"
-                placeholder="Enter a password"
-              />
-              <Form.Text id="passwordHelpBlock" muted>
-                Your password must be min 6 characters long, contain letters and numbers,
-                and must not contain spaces, special characters, or emoji.
-              </Form.Text>
-              {passwordErr && <p>{passwordErr}</p>}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                  minLength="6"
+                  placeholder="Enter a password"
+                />
+                <Form.Text id="passwordHelpBlock" muted>
+                  Your password must be min 6 characters long, contain letters and numbers,
+                  and must not contain spaces, special characters, or emoji.
+                </Form.Text>
+                {passwordErr && <p>{passwordErr}</p>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Label>Email:</Form.Label>
-              <Form.Control
-                type="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                required
-                placeholder="Enter your email address"
-              />
-              {emailErr && <p>{emailErr}</p>}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                  placeholder="Enter your email address"
+                />
+                {emailErr && <p>{emailErr}</p>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBirthday">
-              <Form.Label>Birthday:</Form.Label>
-              <Form.Control
-                type="date"
-                value={birthday}
-                onChange={e => setBirthday(e.target.value)}
-                required
-              />
-            </Form.Group>
-          </Form>
-          <Button className="mb-1" type="submit" onClick={handleRegister}>Register</Button>
-          <Card.Text>
-            Already registered? <Card.Link to={"/"}>Sign in</Card.Link>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </CardGroup>
+              <Form.Group className="mb-3" controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={birthday}
+                  onChange={e => setBirthday(e.target.value)}
+                  required
+                />
+              </Form.Group>
+            </Form>
+            <Button className="mb-1" type="submit" onClick={handleRegister}>Register</Button>
+            <Card.Text>
+              Have an account already? <Link to={"/"}>Sign in</Link>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+    </Container>
   );
 
 }
