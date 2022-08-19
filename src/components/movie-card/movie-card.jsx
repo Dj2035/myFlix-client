a9231cfddbd96b83e5f5a568569a601a0b7a0edc
Dj2035 <dk.jude@yahoo.com>
2022-08-19 14:24:10 +0200

    Update movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Container, Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
 
