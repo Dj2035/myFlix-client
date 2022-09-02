0c9f6045751ce84fcc319a8dae03f095e4666fb4
Dj2035 <dk.jude@yahoo.com>
2022-09-02 20:11:19 +0200

    Add FavMovies component to display list of favorite movies fetched from the App store and mapped to the profile view

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,37 +1,65 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
-import { Button, Row, Col, Figure, Card } from 'react-bootstrap';
-
-import './profile-view.scss';
+import { Button, Col, Figure, Card } from 'react-bootstrap';
 import axios from 'axios';
 
-function FavMovies(props) {
-  const { movie, handleFavorite } = props;
+import './profile-view.scss';
+import { useSelector } from 'react-redux';
 
-  return (
-    <Col xs={12} md={6} lg={3} className="fav-movie">
-      <Figure>
-        <Link to={`/movies/${movie._id}`}>
-          <Figure.Image
-            src={movie.ImagePath}
-            alt={movie.Title}
-            crossOrigin="anonymous"
-          />
-          <Figure.Caption>
-            {movie.Title}
-          </Figure.Caption>
-        </Link>
-      </Figure>
-      <Button
-        variant="outline-danger"
-        className="mt-2 ml-auto"
-        style={{ width: '100%' }}
-        onClick={() => handleFavorite(movie._id, 'remove')}
-      >
-        Remove from ♥️
-      </Button>
-    </Col>
-  )
+function FavMovies(props) {
+
+  const movies = useSelector((state) => state.movies);
+  const favoriteMovies = useSelector((state) => state.user.FavoriteMovies) || [];
+  console.log(favoriteMovies);
+
+  const userFavorites = favoriteMovies.map(function (movie) {
+    return movie._id;
+  })
+  console.log(userFavorites);
+
+
+  const favoriteMoviesList = movies.filter(({ _id }) => favoriteMovies.includes(_id));
+  console.log(favoriteMoviesList);
+
+  const removeFav = (movie) => {
+
+    let token = localStorage.getItem('token');
+    let user = localStorage.getItem('user');
+    console.log(`remove fav auth: ${token}`);
+
+    axios.delete(`https://jude-movie-api.herokuapp.com/users/${user}/movies/${movie._id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        console.log(response);
+        alert(`Movie successfully removed from favorites.`)
+        window.open(`/users/${user}`, '_self');
+      })
+      .catch(error => console.error(error))
+  }
+
+  if (userFavorites.length === 0 || !userFavorites) {
+    return (<p>You have no favorite movies yet.</p>)
+  }
+  else if (userFavorites.length !== 0) {
+    return favoriteMoviesList.map((movie) => (
+      <Col xs={12} md={6} lg={4} xl={3} key={movie._id} className="fav-movie">
+        <Figure>
+          <Link to={`/movies/${movie._id}`}>
+            <Figure.Image
+              src={movie.ImagePath}
+              alt={movie.Title}
+              crossOrigin="anonymous"
+            />
+            <Figure.Caption>
+              {movie.Title}
+            </Figure.Caption>
+          </Link>
+          <Button variant="outline-danger" className="mt-2 ml-auto" style={{ width: '100%' }} onClick={() => removeFav(movie)}>Remove from Favorite</Button>
+        </Figure>
+      </Col>
+    ))
+  }
 }
 
 export default FavMovies;
