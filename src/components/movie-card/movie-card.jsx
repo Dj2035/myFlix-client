a0a5bc1a8a9c41899210376fe5a2ca3b0f0aca25
Dj2035 <dk.jude@yahoo.com>
2022-09-02 20:05:04 +0200

    Update MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,23 +11,14 @@ export class MovieCard extends React.Component {
     const { movie } = this.props;
 
     return (
-      <Col
-        xs={{ span: 8, offset: 1 }}
-        sm={{ span: 6, offset: 2 }}
-        md={{ span: 5, offset: 0 }}
-        lg={4}
-        xl={3}
-        className="mb-3"
-      >
-        <Card className="movieCard" >
-          <Link to={`/movies/${movie._id}`}>
-            <Card.Img crossOrigin="true" variant="top" src={movie.ImagePath} />
-            <Card.Body>
-              <Card.Title>{movie.Title}</Card.Title>
-            </Card.Body>
-          </Link>
-        </Card>
-      </Col>
+      <Card className="movieCard" >
+        <Link to={`/movies/${movie._id}`}>
+          <Card.Img crossOrigin="true" variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+          </Card.Body>
+        </Link>
+      </Card>
     );
   }
 }
