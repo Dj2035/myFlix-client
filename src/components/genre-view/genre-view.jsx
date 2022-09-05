eb6ba20d8bc8f035d900122dadab769016c21f96
Dj2035 <dk.jude@yahoo.com>
2022-09-05 14:26:33 +0200

    Added genre movies

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Button, Row, Col, Card } from "react-bootstrap";
+import { Button, Row, Col, Card, Figure } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./genre-view.scss";
 
 export class GenreView extends React.Component {
   render() {
-    const { genre, onBackClick } = this.props;
+    const { genre, onBackClick, genreMovies } = this.props;
 
     return (
       <>
@@ -23,6 +24,34 @@ export class GenreView extends React.Component {
               <Col className="value">{genre.Description}</Col>
             </Row>
           </Card.Body>
+          <Card.Body>
+            <Row>
+              <Col className="label fw-bold">Some movies with {genre.Name} genre:</Col>
+            </Row>
+          </Card.Body>
+          <Card.Body>
+            <Row>
+              {genreMovies.map((movie) => (
+                <Col xs={12} md={6} lg={4} key={movie._id}>
+                  <Figure>
+                    <Link to={`/movies/${movie._id}`}>
+                      <Figure.Image
+                        src={movie.ImagePath}
+                        alt={movie.Title}
+                        crossOrigin="anonymous"
+                        key={movie._id}
+                        movie={movie}
+                      />
+                      <Figure.Caption>
+                        {movie.Title}
+                      </Figure.Caption>
+                    </Link>
+                  </Figure>
+                </Col>
+              ))}
+            </Row>
+          </Card.Body>
+
           <Card.Footer>
             <Button
               variant="warning"
